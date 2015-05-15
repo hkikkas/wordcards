78b8e22f9a381b10d4594f550d744ccf2e55639c
Hannu <hannu@hot.ee>
2015-05-16 01:50:47 +0300

    * Fixes

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,28 @@
 var wordApp = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
 		
-		// hide the loading message
-		$.mobile.loading().hide();
+		// hide loading message
+		$('.ui-loader').hide();
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
 		
 		// add handlers
-        $('#current_word').on( "swipeleft", wordApp.swipeHandlerLeft );
+		$('#current_word').on( "swipeleft", wordApp.swipeHandlerLeft );
 		$('#current_word').on( "swiperight", wordApp.swipeHandlerRight );
-    },
-	
+	},
 	// receive the swipe events
 	swipeHandlerLeft: function( event ) {
 		$('#current_word').hide();
